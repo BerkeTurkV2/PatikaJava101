70767878d7714e31c18e60ea0da6aa9e603ac2a6
BerkeTurkV2 <forcoding2022v2@gmail.com>
2022-01-19 15:55:29 +0300

    Kdv Tutarı Hesaplayan Program

--- KdvTutarıHesabı.java
+++ KdvTutarıHesabı.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class KdvTutarıHesabı {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Para Tutarını Giriniz: ");
+            int Para = scanner.nextInt();
 
-        System.out.print("Para Tutarını Giriniz: ");
-        int Para = scanner.nextInt();
+            double Kdv = 0.18;
+            double Kdv2 = 0.08;
+            
+            boolean Miktar = Para > 0 && Para <= 1000 ;
 
-        double Kdv = 0.18;
-        double Kdv2 = 0.08;
-        
-        boolean Miktar = Para > 0 && Para <= 1000 ;
+            double KdvTutar = Para *Kdv;
+            double Kdv2Tutar = Para *Kdv2;
 
-        double KdvTutar = Para *Kdv;
-        double Kdv2Tutar = Para *Kdv2;
+            double KdvDurum = Miktar ? KdvTutar : Kdv2Tutar ;
 
-        double KdvDurum = Miktar ? KdvTutar : Kdv2Tutar ;
+            double KdvFiyat = KdvDurum + Para;
 
-        double KdvFiyat = KdvDurum + Para;
-
-        System.out.println("Kdv'siz Fiyat = " + Para);
-        System.out.println("Kdv'li Fiyat = " + KdvFiyat);
-        System.out.println("Kdv Tutarı = " + KdvDurum);
+            System.out.println("Kdv'siz Fiyat = " + Para);
+            System.out.println("Kdv'li Fiyat = " + KdvFiyat);
+            System.out.println("Kdv Tutarı = " + KdvDurum);
+        }
 
     }
 }
