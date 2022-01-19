e1a50746c263ee43867f756cc2f9652ddf68f3e5
BerkeTurkV2 <forcoding2022v2@gmail.com>
2022-01-19 15:55:05 +0300

    Not Ortalaması Hesaplayan Program

--- NotOrtalaması.java
+++ NotOrtalaması.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
 public class NotOrtalaması {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);     
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Matematik notunuzu giriniz:");
+            int Mat = scanner.nextInt();
 
-        System.out.println("Matematik notunuzu giriniz:");
-        int Mat = scanner.nextInt();
+            System.out.println("Fizik notunuzu giriniz:");
+            int Fizik = scanner.nextInt();
 
-        System.out.println("Fizik notunuzu giriniz:");
-        int Fizik = scanner.nextInt();
+            System.out.println("Kimya notunuzu giriniz:");
+            int Kimya = scanner.nextInt();
 
-        System.out.println("Kimya notunuzu giriniz:");
-        int Kimya = scanner.nextInt();
+            System.out.println("Türkçe notunuzu giriniz:");
+            int Türkce = scanner.nextInt();
 
-        System.out.println("Türkçe notunuzu giriniz:");
-        int Türkce = scanner.nextInt();
+            System.out.println("Tarih notunuzu giriniz:");
+            int Tarih = scanner.nextInt();
 
-        System.out.println("Tarih notunuzu giriniz:");
-        int Tarih = scanner.nextInt();
+            System.out.println("Müzik notunuzu giriniz:");
+            int Müzik = scanner.nextInt();
 
-        System.out.println("Müzik notunuzu giriniz:");
-        int Müzik = scanner.nextInt();
+            int Toplam = (Mat + Fizik + Kimya + Türkce + Tarih + Müzik);
+            double Ortalama = Toplam / 6 ;
 
-        int Toplam = (Mat + Fizik + Kimya + Türkce + Tarih + Müzik);
-        double Ortalama = Toplam / 6 ;
+            System.out.println("6 dersin Ortalaması " + Ortalama);
 
-        System.out.println("6 dersin Ortalaması " + Ortalama);
+            boolean Gecti = Ortalama >= 60 ;
 
-        boolean Gecti = Ortalama >= 60 ;
+            String Sonuc = Gecti ? "Sınıfı Geçtiniz..." : "Sınıfta Kaldınız...";
 
-        String Sonuc = Gecti ? "Sınıfı Geçtiniz..." : "Sınıfta Kaldınız...";
-
-        System.out.println(Sonuc);
+            System.out.println(Sonuc);
+        }
 
     }
 }
