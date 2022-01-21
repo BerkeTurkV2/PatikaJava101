b860cd4530790737addf6d16fa691ddaf26269b6
BerkeTurkV2 <forcoding2022v2@gmail.com>
2022-01-21 15:52:55 +0300

    Yıldızlardan elmas oluşturma

---
+++ Yildizlar.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Yildizlar {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Bir sayı giriniz : ");
+            int n = scanner.nextInt();
+            int i,j,k;
+            for(i=1; i<=n; i++){
+                for(j=1; j<=(n-i); j++){
+                    System.out.print(" ");
+                }
+                for(k=1; k<=(2*i)-1;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }  
+            for (i = (n-1); i>=0; i--) {
+                for (j=1; j<=(n-i); j++) {
+                    System.out.print(" ");
+                }
+                for (k=1; k<=(2*i)-1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            } 
+        }
+    }
+}
